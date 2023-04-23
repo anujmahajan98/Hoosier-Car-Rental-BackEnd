6e1e9a120652b604d9a10ba926abd16c2e332b14
rajasimharedd <131618983+rajasimharedd@users.noreply.github.com>
2023-04-23 16:35:38 -0400

    added savePayements and ownercars

---
+++ ownercars.js
@@ -0,0 +1,101 @@
+const mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema.Types
+const uri = "mongodb+srv://anujmaha:aSmaug2022@clustertemp.vgofpu6.mongodb.net/userInfo?retryWrites=true&w=majority";
+//import UserView from '../src/components/UserView/UserView';
+const express = require('express');
+const app = express();
+
+var ownerCarsSchema = new mongoose.Schema({
+    ownerName:{
+            type:String,
+            required: true
+    },
+    carType:{
+        type: String,
+        required: true
+    },
+    carCompany:{
+        type: String,
+        required: true
+    },
+    carModel:{
+        type: String,
+        required: true
+    },
+    startDate:{
+        type: Date,
+        required: true
+    },
+    ownerEmail:{
+        type: String,
+        required: true
+    },
+    carNumber:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    approved:{
+        type: Boolean,
+        required: true
+    },
+    endDate:{
+        type: Date,
+        required: true
+    },
+    
+}, { collection: 'OwnerCarDetails' });
+
+async function getCarInfo(req,res){
+    let carType = req.body.carType;
+    // var startDate = new Date();
+    var startDate = req.body.startDate;
+    //var startDate = new Date(Date.parse(startDate));
+    //var endDate = new Date(Date.parse(endDate));
+    console.log(typeof(startDate));
+    console.log(startDate);
+    // var endDate  = new Date();
+    var endDate = req.body.endDate;
+    // var endDate = new Date(endDate);
+    //console.log(endDate);
+    // startDate = startDate;
+    // endDate = endDate;
+    try{
+    
+      console.log("Fetching data from DB");
+      console.log(carType,startDate,endDate);
+      
+      let days = Math.ceil((new Date(endDate) - new Date(startDate)) / (1000 * 60 * 60 * 24)); // Calculate the difference between startDate and endDate in days
+
+      var docs = await OwnerCars.find({ carType: carType,startDate : {$lte:new Date(startDate)}, endDate:{$gte:new Date(endDate)}, approved:true})
+      .select('ownerName carCompany carModel carType ownerEmail carNumber price').exec();
+    
+    // console.log(docs);
+    const modifiedDocs = docs.map(doc => {
+        if (days <= 1) {
+          doc.price = doc.price * days; // Update the price for rentals less than or equal to 1 day
+        } else {
+          doc.price = doc.price * days; // Update the price for rentals greater than 1 day
+        }
+        return doc;
+      });
+      res.send(modifiedDocs);
+    }
+    catch(error){
+        console.log(error)
+        // console.log("this is destination reached")
+        res.status(404).send("Error");
+    }
+    finally{
+        mongoose.connection.close();
+    }
+};
+
+app.get('/UserView', getCarInfo);
+
+const OwnerCars = mongoose.model('Cars', ownerCarsSchema)
+ 
+module.exports = { OwnerCars, getCarInfo }
--- savePayments.js
+++ savePayments.js
@@ -1,18 +1,42 @@
 var mongoose=require('mongoose');
 const uri = "mongodb+srv://anujmaha:aSmaug2022@clustertemp.vgofpu6.mongodb.net/userInfo?retryWrites=true&w=majority";
-
+// const { CarListingModel } = require('./CarListingModel');
 var savePaymentSchema = new mongoose.Schema({
     PaymentId: {
         type: String,
         required: true,
     },
-    amount: {
-        type: Number,
-        required: true,
+    price: {
+      type: Number,
+      required: true,
     },
-    email: {
+    ownerEmail: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    carModel: {
+      type: String,
+      required: true,
+    },
+    carType: {
+      type: String,
+      required: true,
+    },
+    carNumber: {
         type: String,
         required: true,
+    },
+    carCompany: {
+        type: String,
+        required: true,
+    },
+    ownerName: {
+      type: String,
+      required: true,
     },
     paymentDate: {
         type: Date,
@@ -21,15 +45,31 @@ var savePaymentSchema = new mongoose.Schema({
 }, { collection: 'Payments' });
 
 async function savePayment(req, res) {
-    let id = req.body.id;
-    let amount = req.body.amount;
-    let email = req.body.email;
+    let id = req.body.PaymentId;
+    console.log("savePayment --> id value is",id)
+    let price = req.body.price;
+    console.log("savePayment --> price value is",price)
+    let userEmail = req.body.userEmail;
+    console.log("savePayment --> userEmail value is",userEmail)
+    let ownerEmail = req.body.ownerEmail;
+    let carType = req.body.carType;
+    let carCompany = req.body.carCompany;
+    let carModel = req.body.carModel;
+    let carNumber = req.body.carNumber;
+    let ownerName = req.body.ownerName;
     let paymentDate = req.body.paymentDate;
+    console.log("savePayment --> paymentDate value is",paymentDate)
+    console.log(typeof(paymentDate));
     try {
       await mongoose.connect(uri);
       console.log("Saving payment Info");
-      await savePaymentModel.create({ PaymentId:id, amount: amount, email: email, paymentDate: paymentDate });
-    } catch (error) {
+      await savePaymentModel.create({ PaymentId:id, price: price, userEmail: userEmail, ownerEmail:ownerEmail,
+        carType:carType, carModel:carModel,carCompany:carCompany, carNumber: carNumber, ownerName:ownerName, paymentDate: paymentDate });
+        // const filter = { carNumber: carNumber };
+
+        // const update = { password: NewPassword };
+        // let doc = await ResetPassModel.findOneAndUpdate(filter, update);
+      } catch (error) {
       console.error(error);
     } finally {
       mongoose.connection.close();
